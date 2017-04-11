c75d1bcb3aa3b27ba98bf63aab8e5704d2a6e6cb
Gautham Ilango <gauthamilango@gmail.com>
2017-04-11 16:31:16 +0530

    Updates to Folder entity

--- Gnotes/Folder+CoreDataProperties.swift
+++ Gnotes/Folder+CoreDataProperties.swift
@@ -1,9 +1,9 @@
 //
 //  Folder+CoreDataProperties.swift
-//  Gnotes
+//  
 //
-//  Created by Gautham Ilango on 10/04/17.
-//  Copyright © 2017 Gautham Ilango. All rights reserved.
+//  Created by Gautham Ilango on 11/04/17.
+//
 //
 
 import Foundation
@@ -17,5 +17,6 @@ extension Folder {
     }
 
     @NSManaged public var title: String?
+    @NSManaged public var createdDate: NSDate?
 
 }
